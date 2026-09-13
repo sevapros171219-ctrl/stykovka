8021c8320e0d7f6aa1816dd7725a6b4a4e793d5d
sevapros171219-ctrl <sevapros171219@gmail.com>
2026-09-13 03:58:27 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'stykovka-v1';
-const SHELL = ['./', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+const SHELL = ['./', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
